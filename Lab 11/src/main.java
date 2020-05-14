d97f80540fcb947b70ce6018fa1dad05e6a95580
ichimgabi97 <ichimgabi97@gmail.com>
2020-05-14 19:17:17 +0300

    1

---
+++ Lab 11/src/main.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("Hello world!");
+    }
+}
